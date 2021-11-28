6b01a8f65bf26753602707b213c47c79da0fe696
huiqinhuang <huiqinhuang@tencent.com>
2021-11-28 16:49:55 +0800

    完成主体的功能，接下来就是一些重构的工作

--- DessertStarter/READEME.md
+++ DessertStarter/READEME.md
@@ -10,7 +10,35 @@
 
 ### 知识点
 
+1、 持续复习 Activity 的生命周期
 
+![img](https://developer.android.com/guide/components/images/activity_lifecycle.png)
+
+2、 可以使用 `binding = DataBindingUtil.setContentView(this, R.layout.activity_main)` 直接获取到 `.xml` 的控件和数据，十分方便
+
+3、 `override fun onSaveInstanceState(outState: Bundle)` 保存数据，在下次 `onCreate()` 的时候加载，一般来说是内存不足时需要 `onCreate` 的情况
+
+4、 通过 `data class Dessert(val imageId: Int, val price: Int, val startProductionAmount: Int)` 可以很方便的创建数据类，存储一些数据
 
 ### 遇到的困难
 
+1、 需要在 app 的 `build.gradle` 下添加如下配置，不然没法使用 dataBinding
+
+```groovy
+plugins {
+    id 'kotlin-kapt'
+}
+
+android {
+    buildFeatures {
+        dataBinding = true
+        viewBinding = true
+    }
+}
+```
+
+2、 文件资源未找到
+
+`～/Desktop/git/AndroidStarter/DessertStarter/app/src/main/res/mipmap-anydpi-v26/ic_dessert_clicker.xml:19: AAPT: error: resource drawable/ic_launcher_foreground (aka com.example.dessertstarter:drawable/ic_launcher_foreground) not found.`
+
+将所有  `.xml`  的开头都加上  `<?xml version="1.0" encoding="utf-8"?>`  ，运行一遍，再把报错文件的去掉即可
--- DessertStarter/app/src/main/java/com/example/dessertstarter/MainActivity.kt
+++ DessertStarter/app/src/main/java/com/example/dessertstarter/MainActivity.kt
@@ -1,11 +1,171 @@
+/*
+ * Copyright 2020, The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.example.dessertstarter
 
+import android.content.ActivityNotFoundException
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.core.app.ShareCompat
+import androidx.databinding.DataBindingUtil
+import com.example.dessertstarter.databinding.ActivityMainBinding
 
+const val KEY_REVENUE = "revenue_key"
+const val KEY_DESSERT_SOLD = "dessert_sold_key"
+
+const val TAG = "MainActivity"
+
+// TODO Refactor this code
 class MainActivity : AppCompatActivity() {
+
+    private var revenue = 0
+    private var dessertSold = 0
+
+    private lateinit var binding: ActivityMainBinding
+
+    data class Dessert(val imageId: Int, val price: Int, val startProductionAmount: Int)
+
+    private val allDesserts = listOf(
+        Dessert(R.drawable.cupcake, 5, 0),
+        Dessert(R.drawable.donut, 10, 5),
+        Dessert(R.drawable.eclair, 15, 20),
+        Dessert(R.drawable.froyo, 30, 50),
+        Dessert(R.drawable.gingerbread, 50, 100),
+        Dessert(R.drawable.honeycomb, 100, 200),
+        Dessert(R.drawable.icecreamsandwich, 500, 500),
+        Dessert(R.drawable.jellybean, 1000, 1000),
+        Dessert(R.drawable.kitkat, 2000, 2000),
+        Dessert(R.drawable.lollipop, 3000, 4000),
+        Dessert(R.drawable.marshmallow, 4000, 8000),
+        Dessert(R.drawable.nougat, 5000, 16000),
+        Dessert(R.drawable.oreo, 6000, 20000)
+    )
+
+    private var currentDessert = allDesserts[0]
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        Log.d(TAG, "onCreated Called")
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.dessertButton.setOnClickListener {
+            onDessertClicked()
+        }
+
+        if (savedInstanceState != null) {
+            revenue = savedInstanceState.getInt(KEY_REVENUE, 0)
+            dessertSold = savedInstanceState.getInt(KEY_DESSERT_SOLD, 0)
+            showCurrentDessert()
+        }
+
+        binding.revenue = revenue
+        binding.amountSold = dessertSold
+
+        binding.dessertButton.setImageResource(currentDessert.imageId)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart Called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume Called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause Called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop Called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy Called")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(TAG, "onRestart Called")
+    }
+
+    private fun onDessertClicked() {
+        revenue += currentDessert.price
+        dessertSold++
+
+        binding.revenue = revenue
+        binding.amountSold = dessertSold
+
+        showCurrentDessert()
+    }
+
+    private fun showCurrentDessert() {
+        var newDessert = allDesserts[0]
+
+        for (dessert in allDesserts) {
+            if (dessertSold >= dessert.startProductionAmount) {
+                newDessert = dessert
+            }
+
+            else break
+        }
+
+        if (newDessert != currentDessert) {
+            currentDessert = newDessert
+            binding.dessertButton.setImageResource(newDessert.imageId)
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        Log.d(TAG, "onSaveInstanceState Called")
+        outState.putInt(KEY_REVENUE, revenue)
+        outState.putInt(KEY_DESSERT_SOLD, dessertSold)
+    }
+
+    private fun onShare() {
+        val shareIntent = ShareCompat.IntentBuilder.from(this)
+            .setText(getString(R.string.share_text, dessertSold, revenue))
+            .setType("text/plain")
+            .intent
+        try {
+            startActivity(shareIntent)
+        } catch (ex: ActivityNotFoundException) {
+            Toast.makeText(this, R.string.sharing_not_available, Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.shareMenuButton -> onShare()
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
