fd66f0695ea7ef5fb97df5801df4fdbc790533fa
sunnyswag <hhq126152@gmail.com>
2022-03-07 13:50:06 +0800

    finish static fragment

---
+++ FragmentStarter/README.md
@@ -0,0 +1,13 @@
+
+
+
+
+### REFERENCE
+
+[DEMO，Fragment 的添加](https://blog.csdn.net/zgljl2012/article/details/47423583)
+
+[DEMO，Fragment 的排布](https://www.cnblogs.com/qianguyihao/p/3978989.html)
+
+### 一些坑
+
+IDE 提示使用 FragmentContainerView 标签，但使用这个标签没办法通过 findViewById 获取到 fragment 里边的控件，会报空指针异常
--- FragmentStarter/app/src/main/java/com/demo/fragmentstarter/MainActivity.kt
+++ FragmentStarter/app/src/main/java/com/demo/fragmentstarter/MainActivity.kt
@@ -1,12 +1,22 @@
 
 package com.demo.fragmentstarter
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<View>(R.id.static_fragment)?.apply {
+            findViewById<TextView>(R.id.textView).text = context.getString(R.string.testString)
+        }
+
+        findViewById<View>(R.id.static_fragment1)?.apply {
+            findViewById<TextView>(R.id.textView).text = context.getString(R.string.testString1)
+        }
     }
 }
