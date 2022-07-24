45f71a5fb47c39b8e95dadf5657e54ad7a5fa40e
sunnyswag <hhq126152@gmail.com>
2022-07-24 11:07:47 +0800

    init FindViewById Starter

---
+++ FindViewById/app/src/main/java/com/example/findviewbyid/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.findviewbyid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.example.findviewbyid.Utils.Util
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
---
+++ FindViewById/app/src/main/java/com/example/findviewbyid/Utils/Util.kt
@@ -0,0 +1,33 @@
+package com.example.findviewbyid.Utils
+
+import android.view.View
+import android.view.ViewGroup
+
+object Util {
+
+    /**
+     * 通过 viewId 来找到相应的 View，
+     * 如果存在，则返回 View。如果不存在，则返回 null
+     *
+     * 如果是 ViewGroup ，则递归查找子 View，找到相应的 ID，
+     * 如果是 View ，找到则直接返回
+     * @param viewGroup 在该 viewGroup 中执行查找
+     * @param viewID view 的 ID
+     */
+    fun findViewById(view: View, viewID: Int): View? {
+        if (viewID == view.id) {
+            return view
+        }
+
+        if (view is ViewGroup) {
+            for (i in 0 until view.childCount) {
+                val tmpView = findViewById(view.getChildAt(i), viewID)
+                tmpView?.let {
+                    return tmpView
+                }
+            }
+        }
+
+        return null
+    }
+}
