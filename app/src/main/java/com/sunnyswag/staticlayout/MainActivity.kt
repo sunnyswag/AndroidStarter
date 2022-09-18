2955596fc5b5114ad74cb1e42314e2d2a0e9ae16
sunnyswag <hhq126152@gmail.com>
2022-09-18 18:39:42 +0800

    adjust code for blur article

--- app/src/main/java/com/sunnyswag/staticlayout/MainActivity.kt
+++ app/src/main/java/com/sunnyswag/staticlayout/MainActivity.kt
@@ -1,10 +1,16 @@
 package com.sunnyswag.staticlayout
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.util.Log
+import android.widget.LinearLayout
+import android.widget.RelativeLayout
+import androidx.constraintlayout.widget.ConstraintLayout
+import eightbitlab.com.blurview.BlurView
+import eightbitlab.com.blurview.RenderScriptBlur
 import kotlinx.coroutines.Runnable
 
 class MainActivity : AppCompatActivity() {
@@ -29,35 +35,22 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        seekBar = findViewById<MusicSeekBar>(R.id.musicSeekBar).apply {
-            tryPlay = false
-            tryDuration = Pair(50F, 100F)
-            duration = Pair(0F, 200F)
-            seekProgress = curNum.toFloat()
-            onProgressChanged = { seekBar, progress, fromUser ->
-                Log.d("huiqinhuang", "progress: $progress")
-            }
-        }
+//        seekBar = findViewById<MusicSeekBar>(R.id.musicSeekBar).apply {
+//            tryPlay = false
+//            tryDuration = Pair(50F, 100F)
+//            duration = Pair(0F, 200F)
+//            seekProgress = curNum.toFloat()
+//            onProgressChanged = { seekBar, progress, fromUser ->
+//                Log.d("huiqinhuang", "progress: $progress")
+//            }
+//        }
 
-        handler.postDelayed(runnable, 1000)
-//        val blurView = findViewById<BlurView>(R.id.blurView)
-//        resources.getDrawable(R.drawable.img)
-//        val decorView = window.decorView
-//        val rootView = decorView.findViewById<ViewGroup>(androidx.appcompat.R.id.content)
-//        blurView.setupWith(findViewById(R.id.rootView), RenderScriptBlur(this))
-//            .setOverlayColor(Color.parseColor("#77000000"))
-//            .setBlurRadius(3f)
-
-//        imageView.postDelayed ({
-//            Blurry.with(this)
-//                .color(Color.argb(77, 0, 0, 0))
-//                .animate(500)
-//                .capture(imageView)
-//                .getAsync {
-//                    imageView.setImageDrawable(BitmapDrawable(resources, it))
-//                }
-//
-//        }, 0)
+//        handler.postDelayed(runnable, 1000)
+        val blurView = findViewById<BlurView>(R.id.blurView)
+        val rootView = findViewById<ConstraintLayout>(R.id.rootView)
+        blurView.setupWith(rootView, RenderScriptBlur(this))
+            .setBlurRadius(5F)
+            .setOverlayColor(Color.parseColor("#77000000"))
     }
 
 
